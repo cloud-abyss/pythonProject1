c7ced80e01d9bd09b3726a005fe0f066c0a5b6b6
cloud-abyss <g13639381921@163.com>
2020-09-05 20:40:42 +0800

    2020.9.5

--- practice2.py
+++ practice2.py
@@ -1,4 +1,5 @@
 #1：有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+'''
 count=0  #统计总数
 for g in range(1,5):
     for s in range(1,5):
@@ -60,9 +61,48 @@ rank=[x,y,z]
 index=0
 while index<2:
     if rank[index]>rank[index+1]:
-        l=rank[index]
-        rank[index]=rank[index+1]
-        rank[index+1]=l
+        rank[index],rank[index+1]=rank[index+1],rank[index]
     index+=1
 print(rank)
+'''
+# #4.斐波那契数列
+# #斐波那契数列就是将前两个数字相加会得到第三个数字
+# #迭代
+# n=int(input('请输入想要斐波那契数：'))
+# def feib(n):
+#     if (n==1 or n==2):
+#         return 1
+#     else:
+#         return feib(n-1)+feib(n-2)
+# print(feib(n))
+#
+# #循环
+# def fib(n):
+#     a, b = 1, 1
+#     for i in range(n - 1):
+#         a, b = b, a + b
+#     return a
+#
+# print(fib(3))
+
+# #题目5：将一个列表的数据复制到另一个列表中。
+# c1=[1,2,'tec','lll']
+# c2=c1.copy()
+# print(c2)
+
+# #题目6：输出 9*9 乘法口诀表。
+# # 程序分析：分行与列考虑，共9行9列，i控制行，j控制列。
+# for r in range(1,10):
+#     for c in range(1,r+1):
+#         print('{}*{}={}'.format(r,c,r*c),end='\t')
+#     print('\n')
+
+# #题目7：暂停一秒输出。
+# #程序分析：使用 time 模块的 sleep() 函数。
+# import time
+#
+# dict1={1:'a',2:'b',3:'c'}
+# for key,value in dict.items(dict1):
+#     print(value)
+#     time.sleep(2)
 
